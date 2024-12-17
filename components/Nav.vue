dd36a95bbd53aec5470f79df43c25c7bad700ff1
scottkelly36 <scottkelly36@gmail.com>
2024-12-17 20:21:36 +0000

    adds assets and starts nav layout

---
+++ components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="nav__container">
+    <div class="nav__container-inner">
+        <div class="nav__logo-container">
+            <img src="../assets/logos/icon.png" alt=""/>
+        </div>
+        <div class="nav__links-container">
+
+        </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .nav__container{
+        width: 100%;
+        background-color: #f4f4f4;
+        height: fit-content;
+    }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import Nav from '~/components/Nav.vue';
+</script>
+
 <template>
     <div>
+        <Nav/>
         <slot/>
     </div>
 </template>
+<style>
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+</style>
