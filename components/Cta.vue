d60bbd98bdef4ae7a274d05c3c3a70a4f0936804
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2024-12-20 18:07:30 +0000

    updates images and fonts

---
+++ components/Cta.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="ftb-widget" :data-id="widgetId" :data-token="widgetToken"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    widgetId: {
+      type: String,
+      required: true,
+    },
+    widgetToken: {
+      type: String,
+      required: true,
+    },
+  },
+  mounted() {
+    // Dynamically load the external script
+    const script = document.createElement('script');
+    script.src = 'https://widget.freetobook.com/widget.js?v=20190925';
+    script.async = true;
+    script.onload = () => {
+      console.log('FreeToBook widget loaded');
+    };
+    document.body.appendChild(script);
+  },
+};
+</script>
+
+<style></style>
