f19d6e13fe848e87430bbfcd6b51977c883faa97
scottkelly36 <scottkelly36@gmail.com>
2025-01-01 22:54:28 +0000

    sets up reusable icon component

---
+++ components/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="iconComponent" />
+</template>
+
+<script setup>
+import Steps from "./Icons/Steps.vue";
+import Smoking from "./Icons/Smoking.vue";
+import Pets from "./Icons/Pets.vue";
+import Parties from "./Icons/Parties.vue";
+
+const props = defineProps({
+  iconName: { type: String, required: true },
+});
+
+const iconMap = {
+  Steps,
+  Smoking,
+  Pets,
+  Parties,
+};
+
+const iconComponent = iconMap[props.iconName] || null;
+</script>
