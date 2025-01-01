63da5a4eff4e0d7f86e0ec7a2938cf7235c1de3d
scottkelly36 <scottkelly36@gmail.com>
2025-01-01 21:16:58 +0000

    Accordion setup

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -1,13 +1,84 @@
 <template>
-  
+  <AccordionList class="accordion-list">
+    <AccordionItem
+      v-for="(accordionItem, index) in componentData?.items"
+      title="First Item"
+      :key="index"
+      class="accordion-item"
+    >
+      <template #summary>{{ accordionItem.title }} </template>
+      <template #icon>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-down"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+          />
+        </svg>
+      </template>
+      <ul class="accordion-item__list-container">
+        <li
+          v-for="(bullet, index) in accordionItem?.bullets"
+          :key="index"
+          class="accordion-item__list-item"
+        >
+          {{ bullet }}
+        </li>
+      </ul>
+    </AccordionItem>
+  </AccordionList>
 </template>
 
-<script>
-export default {
+<script setup>
+import { AccordionList, AccordionItem } from "vue3-rich-accordion";
 
-}
+const props = defineProps({
+  componentData: { type: Object, required: true },
+});
+
+console.log(props);
 </script>
 
 <style>
+.accordion-list {
+  margin: 0 15px;
+}
+.accordion-item {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  border-bottom: solid black 1px;
+  transition: all 0.5s ease;
+}
 
+.accordion-item .accordion-item__summary {
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.accordion-item .accordion-item__summary .accordion-item__summary-title {
+  font-size: 16px;
+  text-transform: uppercase;
+}
+
+.accordion-item__summary-icon {
+  display: flex;
+  align-items: center;
+  transition: all 0.3s ease;
+}
+
+.accordion-item--open .accordion-item__summary-icon {
+  rotate: 180deg;
+}
+.accordion-item__list-container {
+  margin: 10px 20px;
+  list-style-type: disc;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="page-layout">
-    <Hero :componentData="data.hero"/>
+    <Hero :componentData="data.hero" />
     <text-split :componentData="data.textSplit1" />
     <parallax />
     <text-split :componentData="data.textSplit2" />
-    <Carousel :componentData="data.carousel"/>
+    <Carousel :componentData="data.carousel" />
     <text-split :componentData="data.textSplit3" />
+    <Accordion :componentData="data.Accordion" />
     <Footer :componentData="data.footer" />
   </div>
 </template>
 
 <script setup>
-import pageData from '../assets/content.json';
-import Footer from '~/components/Footer.vue';
+import pageData from "../assets/content.json";
+import Footer from "~/components/Footer.vue";
 
 const data = pageData;
 </script>
