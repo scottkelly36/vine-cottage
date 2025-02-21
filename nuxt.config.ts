da89ad009ffd86065db1d60a168862020ad7c527
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2025-02-21 14:06:24 +0000

    define a port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  plugins: [
-    { src: '~/plugins/vue-scroll-reveal.client.js', mode: 'client' },
-  ],
-  devtools: { enabled: true },
-})
+  plugins: [{ src: '~/plugins/vue-scroll-reveal.client.js', mode: 'client' }],
+  devtools: { enabled: false },
+  server: {
+    port: 3000, // Define the port for your app
+  },
+});
